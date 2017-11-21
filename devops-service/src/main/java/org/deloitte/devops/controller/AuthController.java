781348bf3d6cd136742e541fb2811dd3fdda5862
Surya <surya_vikas525@yahoo.com>
2017-11-21 17:27:32 +0530

    initial commit v1 with working model of jira auth

--- devops-service/src/main/java/org/deloitte/devops/controller/AuthController.java
+++ devops-service/src/main/java/org/deloitte/devops/controller/AuthController.java
@@ -1,37 +1,27 @@
 package org.deloitte.devops.controller;
 
-import java.io.BufferedReader;
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathConstants;
-import javax.xml.xpath.XPathExpression;
 import javax.xml.xpath.XPathExpressionException;
-import javax.xml.xpath.XPathFactory;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.ByteArrayEntity;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
-import org.springframework.stereotype.Controller;
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.w3c.dom.Document;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.xml.sax.SAXException;
 
-@Controller
+@RestController
 public class AuthController {
 	
 /*static final Properties properties = new Properties();
@@ -51,69 +41,31 @@ public class AuthController {
 	
 
 	@RequestMapping(value = "/auth", method = RequestMethod.GET, headers = "Accept=application/json")
-	public String authUser() throws ClientProtocolException, IOException, ParserConfigurationException,
-			SAXException, XPathExpressionException {
-		String authUrl = "https://tabtest2.clearlight.deloitteinnovation.us/api/2.4/auth/signin";
-		String token = null;
-		String site_id = null;
+	public @ResponseBody String authUser() throws ClientProtocolException, IOException, ParserConfigurationException,
+			SAXException, XPathExpressionException, JSONException {
+		
+		
+		
+		String authUrl = "https://trackotrack.atlassian.net/rest/auth/1/session";
+
 		HttpClient authClient = HttpClientBuilder.create().build(); 
 		HttpPost post = new HttpPost(authUrl);
-		String pass = "e8Uj@w)&quot;Z5";
-		String name = "sgarripelly";
-		String contentUrl = "IAAP";
-		String xml = "<tsRequest><credentials name='" + name + "' password='" + pass + "'><site contentUrl='"
-				+ contentUrl + "'/></credentials></tsRequest>";
-		HttpEntity entity = new ByteArrayEntity(xml.getBytes("UTF-8"));
-		post.setHeader("Accept", "application/xml");
-		post.setHeader("Content-type", "application/xml");
+		
+		JSONObject jObject = new JSONObject();
+		jObject.put("username", "neelsindwani@gmail.com");
+		jObject.put("password", "Nov@2017ns");
+		
+		String userandpass = jObject.toString();
+
+		HttpEntity entity = new ByteArrayEntity(userandpass.getBytes());
+		post.setHeader("Accept", "application/json");
+		post.setHeader("Content-type", "application/json");
 		post.setEntity(entity);
 		HttpResponse response = authClient.execute(post);
 		String result = EntityUtils.toString(response.getEntity());
+System.out.println("rest.."+result);
 
-		// parse response xml string...
-		DocumentBuilderFactory f = DocumentBuilderFactory.newInstance();
-		DocumentBuilder b = f.newDocumentBuilder();
-		Document doc = b.parse(new ByteArrayInputStream(result.getBytes("UTF-8")));
-
-		XPathFactory xPathfactory = XPathFactory.newInstance();
-		XPath xpath = xPathfactory.newXPath();
-
-		// path to get token
-		XPathExpression expr = xpath.compile("//tsResponse/credentials[@token]");
-		NodeList nl = (NodeList) expr.evaluate(doc, XPathConstants.NODESET);
-
-		// path to get site id
-		XPathExpression expr1 = xpath.compile("//tsResponse/credentials/site[@id]");
-		NodeList nl1 = (NodeList) expr1.evaluate(doc, XPathConstants.NODESET);
-
-		for (int i = 0; i < nl.getLength(); i++) {
-			Node currentItem = nl.item(i);
-			token = currentItem.getAttributes().getNamedItem("token").getNodeValue();
-
-			Node currentItem1 = nl1.item(i);
-			site_id = currentItem1.getAttributes().getNamedItem("id").getNodeValue();
-
-		}
-
-		String allViewsUrl = "https://tabtest2.clearlight.deloitteinnovation.us/api/2.4/sites/" + site_id + "/views";
-
-		// subsequent GET call for all views
-		HttpClient viewsClient = HttpClientBuilder.create().build();
-		HttpGet request = new HttpGet(allViewsUrl);
-
-		// add request header
-		request.addHeader("X-Tableau-Auth", token);
-		HttpResponse getResponse = viewsClient.execute(request);
-
-		BufferedReader rd = new BufferedReader(new InputStreamReader(getResponse.getEntity().getContent()));
-
-		StringBuffer strResult = new StringBuffer();
-		String line = "";
-		while ((line = rd.readLine()) != null) {
-			strResult.append(line);
-		}
-
-		return "redirect:/index.jsp";
+		return result;
 	}
 	
 	@RequestMapping(value = "/index", method = RequestMethod.GET)
