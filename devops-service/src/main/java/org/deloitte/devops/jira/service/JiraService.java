691698091e9db9d94d3b3dd96b441fe752a0955c
sofahmed <sofahmed@USHYDSOFAHMED2.us.deloitte.com>
2018-01-22 18:45:01 +0530

    bug

--- devops-service/src/main/java/org/deloitte/devops/jira/service/JiraService.java
+++ devops-service/src/main/java/org/deloitte/devops/jira/service/JiraService.java
@@ -67,7 +67,8 @@ public class JiraService {
                 Fields fields =new Fields();
                 fields.setSummary(responseJson.getString("summary"));
                 fields.setDescription(responseJson.getString("description"));
-              fields.setCustomfield_10101(responseJson.getString("customfield_10101"));
+           
+                fields.setCustomfield_10101(responseJson.optString("customfield_10101"));
               fields.setJobName(fields.getCustomfield_10101());
                 Gson gson = new Gson();
                 List<Issue> list =
